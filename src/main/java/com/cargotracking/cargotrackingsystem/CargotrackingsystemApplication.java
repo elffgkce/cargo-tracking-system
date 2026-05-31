f0ee425221c37688a3ffc13cc5f797dd09e47afb
Elif <elffgkce5@gmail.com>
2026-05-31 21:44:48 +0300

    Initial commit: Cargo Tracking System project initialized with Spring Boot

---
+++ src/main/java/com/cargotracking/cargotrackingsystem/CargotrackingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.cargotracking.cargotrackingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargotrackingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargotrackingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargotracking/cargotrackingsystem/CargotrackingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargotracking.cargotrackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargotrackingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
